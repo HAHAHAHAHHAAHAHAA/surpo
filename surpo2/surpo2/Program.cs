9f2b2caa2d69b6907d571c069ff91900b08e405f
annaantipovaa <antipova2004anna@mail.ru>
2023-11-15 19:34:26 +0300

    added user input

--- surpo2/surpo2/Program.cs
+++ surpo2/surpo2/Program.cs
@@ -6,8 +6,9 @@ namespace surpo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("хихихихих");
+            Console.WriteLine("Введите какую-нибудь фразу:");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
         }
     }
 }
