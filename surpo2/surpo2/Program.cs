7a9d3ec9d830b96945ad3910c7b05d25f08be29c
annaantipovaa <antipova2004anna@mail.ru>
2023-11-19 16:22:35 +0300

    added Sleep

--- surpo2/surpo2/Program.cs
+++ surpo2/surpo2/Program.cs
@@ -48,5 +48,10 @@ namespace surpo2
             Console.WriteLine("I'm working >_<");
         }
 
+        public static void Sleep()
+        {
+            Console.WriteLine("Going to sleep");
+        }
+
     }
 }
